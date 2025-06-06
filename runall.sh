05e8f25aaf30713ec61f4b858f477b3b1138a6ec
zhoutz <tz.zhou@foxmail.com>
2025-06-07 05:07:19 +0800

    add additional algorithms to runall script

--- runall.sh
+++ runall.sh
@@ -1,4 +1,6 @@
 python3 p/bench.py inte_r && python3 p/p_err_time.py inte_r svg
 python3 p/bench.py inte_p && python3 p/p_err_time.py inte_p svg
 python3 p/bench.py inte_h && python3 p/p_err_time.py inte_h svg
+python3 p/bench.py inte_xi && python3 p/p_err_time.py inte_xi svg
+python3 p/bench.py inte_uvy_p && python3 p/p_err_time.py inte_uvy_p svg
  
